9b1b24985c4356e252ad6c4e44dee0d1d6251986
pchabot2021 <patrickchabot103@gmail.com>
2025-01-29 22:03:06 -0500

    more changes for permissions

--- storage.py
+++ storage.py
@@ -1,8 +1,6 @@
 from google.cloud import datastore, storage
 import os
 import time
-from flask import Flask, request, render_template, redirect
-from werkzeug.utils import secure_filename
 
 PROJECT_ID = os.getenv("GOOGLE_CLOUD_PROJECT", "sonic-base-449423-g4")
 
@@ -12,50 +10,16 @@ storage_client = storage.Client(project=PROJECT_ID)
 BUCKET_NAME = "chabotproject1bucket"
 
 def upload_file(bucket_name, file_stream, destination_filename):
-    """Uploads file to Google Cloud Storage."""
+    """Uploads file to Google Cloud Storage and returns a public URL."""
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(destination_filename)
     blob.upload_from_file(file_stream)
-    return f"gs://{bucket_name}/{destination_filename}"
+    
+    # Return the public URL for the uploaded file
+    return f"https://storage.googleapis.com/{bucket_name}/{destination_filename}"
 
 def add_db_entry(object):
     """Stores file metadata in Google Cloud Datastore."""
     entity = datastore.Entity(key=datastore_client.key('photos'))
     entity.update(object)
     datastore_client.put(entity)
-
-# Flask App
-app = Flask(__name__)
-
-@app.get("/")
-def index():
-    """Fetches and displays uploaded photos."""
-    query = datastore_client.query(kind='photos')
-    photos = list(query.fetch())
-    return render_template('index.html', photos=photos)
-
-@app.post("/upload")
-def upload_image():
-    """Handles image uploads and stores metadata."""
-    if 'file' not in request.files:
-        return redirect('/')
-    
-    file = request.files['file']
-    if file.filename == '':
-        return redirect('/')
-    
-    if file:
-        filename = secure_filename(file.filename)
-        gcs_path = upload_file(BUCKET_NAME, file, filename)
-        
-        entity = {
-            "name": filename,
-            "gcs_path": gcs_path,
-            "timestamp": int(time.time())
-        }
-        add_db_entry(entity)
-
-    return redirect('/')
-
-if __name__ == "__main__":
-    app.run(host="localhost", port=8080, debug=True)
